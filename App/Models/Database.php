90bad51766cc64382d469ef91e5de3d93a4f2253
hamzalafsioui <hamzalafsioui@gmail.com>
2026-01-19 17:15:24 +0100

    Implement Database [Singlton]

--- App/Models/Database.php
+++ App/Models/Database.php
@@ -21,10 +21,10 @@ class Database
                 $host = defined('DB_HOST') ? DB_HOST : 'localhost';
                 $port = defined('DB_PORT') ? DB_PORT : '5432';
                 $user = defined('DB_USER') ? DB_USER : 'postgres';
-                $pass = defined('DB_PASS') ? DB_PASS : 'password';
-                $name = defined('DB_NAME') ? DB_NAME : 'club_db';
+                $pass = defined('DB_PASS') ? DB_PASS : 'Sa@123456';
+                $name = defined('DB_NAME') ? DB_NAME : 'clubEdge_db';
 
-                // Ensure DSN uses these variables for PostgreSQL
+                // variables for PostgreSQ
                 $dsn = "pgsql:host={$host};port={$port};dbname={$name}";
 
                 self::$instance = new PDO($dsn, $user, $pass, [
