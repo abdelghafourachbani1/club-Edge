0e19099dbc1d5b4b2a51c708e558f69036fae947
hamzalafsioui <hamzalafsioui@gmail.com>
2026-01-20 12:01:23 +0100

    add example only for testing

---
+++ App/Controllers/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+use Controller;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        
+        echo "Admin Dashboard";
+    }
+
+    public function example()
+    {
+        $data = [
+            'title' => 'Admin Example Page',
+            'message' => 'This is an example page in the Admin panel.'
+        ];
+        $this->renderWithLayout('admin/example', $data);
+    }
+    
+
+   
+}
