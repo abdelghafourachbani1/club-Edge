a95ca4b06214a989031b00ed1f882a42d65062c8
hamzalafsioui <hamzalafsioui@gmail.com>
2026-01-20 11:59:01 +0100

    ClubController.php

--- App/Controllers/ClubController.php
+++ App/Controllers/ClubController.php
@@ -4,15 +4,14 @@ namespace App\Controllers;
 
 use Controller;
 use App\Repositories\EventRepository;
-class ClubController extends Controller{
+
+class ClubController extends Controller
+{
 
     private EventRepository $eventRepository;
 
     public function __construct()
     {
         $this->eventRepository = new EventRepository();
-    
     }
-
-
 }
