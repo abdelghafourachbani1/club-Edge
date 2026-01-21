46e84fed89bb8e52c4db8fdab82c9a06d0edb125
hamzalafsioui <hamzalafsioui@gmail.com>
2026-01-21 15:01:34 +0100

    add LOG_PATH

--- config/App.php
+++ config/App.php
@@ -6,15 +6,16 @@ if (session_status() === PHP_SESSION_NONE) {
 
 // Include autoloader for automatic class loading
 require_once __DIR__ . '/autoload.php';
-// require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // Database credentials for PostgreSQL
 define('DB_HOST', 'localhost');
 define('DB_PORT', '5432');
 define('DB_USER', 'postgres');
-define('DB_PASS', 'password');
+define('DB_PASS', 'Sa@123456');
 define('DB_NAME', 'clubEdge');
 
 // App URL
 define('BASE_URL', rtrim('http://localhost/club-Edge', '/'));
 define('BASE_PATH', dirname(__DIR__));
+define('LOG_PATH', BASE_PATH . '/logs');
