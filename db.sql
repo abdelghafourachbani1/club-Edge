2438b678744d6130f4c7aafbe29a1d6e422c1b27
hamzalafsioui <hamzalafsioui@gmail.com>
2026-01-24 22:44:40 +0100

    db.sql add is_admin

--- db.sql
+++ db.sql
@@ -6,7 +6,8 @@ CREATE TABLE users (
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE
 );
 
 
